815d42e78fea6c2951845f92319043b51312a518
LottaHarmonen <143486591+LottaHarmonen@users.noreply.github.com>
2023-11-13 14:30:50 +0100

    Add files via upload

---
+++ SQLÖvningarLotta/SQLQuery1.sql
@@ -0,0 +1,22 @@
+--CREATE DATABASE DjurDb;
+USE KlassenDb
+
+--CREATE TABLE KlassenTbl (
+-- Id int PRIMARY KEY,
+-- Förnamn nvarchar(max) NOT NULL,
+-- Efternamn nvarchar(max) NOT NULL,
+-- Ålder int,
+-- Hemort nvarchar(max) NOT NULL
+--);
+
+--SELECT * FROM KlassenTbl;
+
+--Insert Into KlassenTbl (Id, Förnamn, EfterNamn, Ålder, Hemort)
+--Values ('5', 'Sebastian', 'Moya', '37', 'Göteborg');
+
+--SELECT * FROM DjurTbl
+--WHERE Namn LIKE '%a%';
+
+
+
+
